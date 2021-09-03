c0c8d93e52fc5b8f609f93abff0a309d21e19b8a
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-04 03:45:28 +0600

    Added the command loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js');
+const fs = require('fs');
 require('dotenv').config();
 
 const client = new Client({
@@ -6,6 +7,34 @@ const client = new Client({
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES
     ]
+});
+client.commands = new Collection();
+client.aliases = new Collection();
+
+fs.readdir('./src/commands', (err, commandFolders) => {
+    if (err) console.log(err);
+
+    commandFolders.forEach(commandFolder => {
+        fs.readdir(`./src/commands/${commandFolder}`, (err, files) => {
+            if (err) console.log(err);
+
+            const jsfiles = files.filter(f => f.endsWith('.js'));
+            if (jsfiles.length <= 0) {
+                console.log(`No commands found in ${commandFolder}`);
+                return;
+            }
+
+            jsfiles.forEach(jsfile => {
+                console.log(jsfile);
+                const cmd = require(`./commands/${commandFolder}/${jsfile}`);
+                client.commands.set(cmd.info.name, cmd);
+                cmd.info.aliases.forEach(alias => {
+                    client.aliases.set(alias, cmd.info.name);
+                })
+                console.log(`${jsfile} loaded!`);
+            });
+        });
+    });
 });
 
 client.on('ready', async () => {
