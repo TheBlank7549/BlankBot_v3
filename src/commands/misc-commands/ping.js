57ebf7a13c0e066b85fc53b65af58817b84fd3d4
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-04 16:32:22 +0600

    Removed unnecessary args requirements

--- src/commands/misc-commands/ping.js
+++ src/commands/misc-commands/ping.js
@@ -1,8 +1,8 @@
 module.exports.info = {
     name: 'ping',
     aliases: [],
-    minArgs: 0,
-    maxArgs: 99,
+    description: 'Shows the latency of the bot',
+    usage: 'ping'
 };
 
 module.exports.run = async (client, msg, args) => {
@@ -12,11 +12,5 @@ module.exports.run = async (client, msg, args) => {
         m.edit({
             content: `Pong, but \`${m.createdTimestamp - msg.createdTimestamp}ms\` later!`
         });
-    });
-};
-
-module.exports.argsErr = (msg, prefix) => {
-    msg.channel.send({
-        content: `Incorrect number of arguments provided, correct usage:\n\`${prefix}ping\``
     });
 };
