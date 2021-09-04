84e025fa01f3ee3786798ca296e716dfaccee273
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-04 13:41:40 +0600

    Added the messageCreate event handler

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const { Client, Intents, Collection } = require('discord.js');
 const fs = require('fs');
 require('dotenv').config();
 
+const { defaultPrefix } = require('../config.json');
+let prefix = defaultPrefix;
+
 const client = new Client({
     intents: [
         Intents.FLAGS.GUILDS,
@@ -18,19 +21,18 @@ fs.readdir('./src/commands', (err, commandFolders) => {
         fs.readdir(`./src/commands/${commandFolder}`, (err, files) => {
             if (err) console.log(err);
 
+            console.log(`Loading files from ${commandFolder}`);
             const jsfiles = files.filter(f => f.endsWith('.js'));
             if (jsfiles.length <= 0) {
-                console.log(`No commands found in ${commandFolder}`);
-                return;
-            }
+                return console.log(`No commands found in ${commandFolder}`);
+            };
 
             jsfiles.forEach(jsfile => {
-                console.log(jsfile);
                 const cmd = require(`./commands/${commandFolder}/${jsfile}`);
                 client.commands.set(cmd.info.name, cmd);
                 cmd.info.aliases.forEach(alias => {
                     client.aliases.set(alias, cmd.info.name);
-                })
+                });
                 console.log(`${jsfile} loaded!`);
             });
         });
@@ -39,6 +41,36 @@ fs.readdir('./src/commands', (err, commandFolders) => {
 
 client.on('ready', async () => {
     console.log('BlankBot_v3 is now online');
+});
+
+client.on('messageCreate', async msg => {
+    if (msg.author.bot) return;
+    if (msg.author.id === client.user.id) return;
+
+
+    if (msg.content.startsWith(prefix)) {
+        const args = msg.content.slice(prefix.length).toLowerCase().trim().split(/ /g);
+        const cmdUsed = args.shift();
+        let command;
+
+        if (client.commands.has(cmdUsed)) {
+            command = client.commands.get(cmdUsed);
+        } else if (client.aliases.has(cmdUsed)) {
+            command = client.commands.get(client.aliases.get(cmdUsed));
+        } else {
+            return;
+        }
+
+        try {
+            if (args.length < command.info.minArgs || args.length > command.info.maxArgs) {
+                return command.argsErr(msg, prefix);
+            } else {
+                command.run(client, msg, args);
+            };
+        } catch (error) {
+            console.log(error);
+        };
+    };
 });
 
 client.login(process.env.BOTTOKEN);
