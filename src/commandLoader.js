a630234f20059db0999777083c31ded7ba3795e5
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-05 17:12:58 +0600

    Added comments for clarification

--- src/commandLoader.js
+++ src/commandLoader.js
@@ -1,18 +1,24 @@
+// Importing the fs node package
 const fs = require('fs');
+
 module.exports = client => {
+    // Getting sub-folders from the main commands folder
     fs.readdir('./src/commands', (err, commandFolders) => {
         if (err) console.log(err);
 
         commandFolders.forEach(commandFolder => {
+            // Getting files from the sub-folders
             fs.readdir(`./src/commands/${commandFolder}`, (err, files) => {
                 if (err) console.log(err);
 
+                // Filtering the js and non-js files
                 console.log(`Loading files from ${commandFolder}`);
                 const jsfiles = files.filter(f => f.endsWith('.js'));
                 if (jsfiles.length <= 0) {
                     return console.log(`No commands found in ${commandFolder}`);
                 };
 
+                // Adding the command names, aliases and functions to the appropriate collections
                 jsfiles.forEach(jsfile => {
                     const cmd = require(`./commands/${commandFolder}/${jsfile}`);
                     client.commands.set(cmd.info.name, cmd);
