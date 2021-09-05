1e1c69b707feabbcd0b5083241d51bad4223dbc8
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-05 21:08:08 +0600

    Added checks for requirements n restricted commands

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -1,13 +1,14 @@
-module.exports = (client, msg, prefix) => {
+module.exports = async (client, msg, prefix) => {
+    // Returning if the msg author is a bot
     if (msg.author.bot) return;
-    if (msg.author.id === client.user.id) return;
 
-
-    if (msg.content.startsWith(prefix)) {
+    // Filters out msges not starting with the prefix, otherwise preparing args and cmdUsed
+    if (msg.content.toLowerCase().startsWith(prefix)) {
         const args = msg.content.slice(prefix.length).toLowerCase().trim().split(/ /g);
         const cmdUsed = args.shift();
         let command;
 
+        // Gets the command being used(if it exists)
         if (client.commands.has(cmdUsed)) {
             command = client.commands.get(cmdUsed);
         } else if (client.aliases.has(cmdUsed)) {
@@ -17,7 +18,27 @@ module.exports = (client, msg, prefix) => {
         };
 
         try {
-            const { minArgs, maxArgs, usage } = command.info
+            const { category, minArgs, maxArgs, usage } = command.info
+
+            // Checks for requirements if the command is from a restricted category
+            if (category === 'owner' && msg.author.id !== '713019901333340192') {
+                await msg.channel.send({
+                    content: `<@${msg.author.id}> You do not have permission to use this command`
+                }).then(reply => {
+                    setTimeout(() => reply.delete(), 10000);
+                });
+                return;
+            };
+            if (category === 'admin' && !msg.member.permissions.has('ADMINISTRATOR')) {
+                await msg.channel.send({
+                    content: `<@${msg.author.id}> You do not have permission to use this command.\nContact a server administrator if needed`
+                }).then(reply => {
+                    setTimeout(() => reply.delete(), 10000);
+                });
+                return;
+            };
+
+            // Checks if any arg count requirements are met
             if (minArgs !== undefined && args.length < minArgs || maxArgs !== undefined && args.length > maxArgs) {
                 return msg.channel.send({
                     content: `Incorrect number of arguments provided, correct usage:\n\`${prefix}${usage}\``
