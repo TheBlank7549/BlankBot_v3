fc8e61cb537fefa2b79b88282570079b698f4f87
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-10 23:55:42 +0600

    Added say.js

---
+++ src/commands/misc-commands/say.js
@@ -0,0 +1,18 @@
+const logger = require('../../functions/logger.js');
+
+module.exports.info = {
+    name: 'say',
+    alises: ['tell', 'speak'],
+    category: "misc",
+    minArgs: 1,
+    description: 'Says something on your behalf',
+    usage: 'say <text>'
+};
+
+module.exports.run = async (client, msg, args) => {
+    const text = msg.content.split(/ +/g).slice(1).join(' ');
+
+    msg.channel.send({
+        content: text
+    }).then(() => msg.delete())
+};
