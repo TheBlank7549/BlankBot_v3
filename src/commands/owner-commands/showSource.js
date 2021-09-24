a3a4f8cebc0916f21cbe144c7b14527620a885ec
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-09-24 20:47:45 +0600

    Added showFile.js

---
+++ src/commands/owner-commands/showSource.js
@@ -0,0 +1,37 @@
+const logger = require('../../functions/logger.js');
+const fs = require('fs');
+
+module.exports.info = {
+    name: 'showfile',
+    aliases: ['sharefile'],
+    category: "owner",
+    minArgs: 1,
+    description: 'Shows specified source files',
+    usage: 'showFile <filePath>'
+};
+
+module.exports.run = async (client, msg, args) => {
+    const path = `src/${args[0]}`;
+
+    // Gets the file name ending with ".js"
+    const fileName = path.match(/(\w+.js)$/);
+
+    // Checks if the file exists AND ends in ".js"
+    if (!fileName || !fs.existsSync(path)) {
+        msg.channel.send({
+            content: `\`${path}\` is not a valid js file`
+        }).catch(console.error);
+        logger.logFailedCmd(client, msg);
+    };
+
+    // Sends the specified file as an attachment
+    msg.channel.send({
+        files: [
+            {
+                attachment: path,
+                name: fileName[1]
+            }
+        ]
+    }).catch(console.error);
+    logger.logSuccessfulCmd(client, msg);
+};
