e44ec73464a644ca2b1f4a0418974f6fe6fb594c
TheBlank7549 <89746692+TheBlank7549@users.noreply.github.com>
2021-10-11 00:42:05 +0600

    Added the missing return causing crashing

--- src/commands/owner-commands/showFile.js
+++ src/commands/owner-commands/showFile.js
@@ -2,36 +2,37 @@ const logger = require('../../functions/logger.js');
 const fs = require('fs');
 
 module.exports.info = {
-    name: 'showfile',
-    aliases: ['sharefile'],
-    category: "owner",
-    minArgs: 1,
-    description: 'Shows specified source files',
-    usage: 'showFile <filePath>'
+  name: 'showfile',
+  aliases: ['sharefile'],
+  category: "owner",
+  minArgs: 1,
+  description: 'Shows specified source files',
+  usage: 'showFile <filePath>'
 };
 
 module.exports.run = async (client, msg, args) => {
-    const path = `src/${args[0]}`;
+  const path = `src/${args[0]}`;
 
-    // Gets the file name ending with ".js"
-    const fileName = path.match(/(\w+.js)$/);
+  // Gets the file name ending with ".js"
+  const fileName = path.match(/(\w+.js)$/);
 
-    // Checks if the file exists AND ends in ".js"
-    if (!fileName || !fs.existsSync(path)) {
-        msg.channel.send({
-            content: `\`${path}\` is not a valid js file`
-        }).catch(console.error);
-        logger.logFailedCmd(client, msg);
-    };
-
-    // Sends the specified file as an attachment
+  // Checks if the file exists AND ends in ".js"
+  if (!fileName || !fs.existsSync(path)) {
     msg.channel.send({
-        files: [
-            {
-                attachment: path,
-                name: fileName[1]
-            }
-        ]
+      content: `\`${args[0]}\` is not a valid js file`
     }).catch(console.error);
-    logger.logSuccessfulCmd(client, msg);
+    logger.logFailedCmd(client, msg);
+    return;
+  };
+
+  // Sends the specified file as an attachment
+  msg.channel.send({
+    files: [
+      {
+        attachment: path,
+        name: fileName[1]
+      }
+    ]
+  }).catch(console.error);
+  logger.logSuccessfulCmd(client, msg);
 };
